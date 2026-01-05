512f71a33e44ac73a6eec09d2e01c44594e4b2a4
Shrihari Kale <163184413+shrihari-kale@users.noreply.github.com>
2026-01-05 16:55:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const submitBtn = document.querySelector("#submit");
+const existingBtn = document.querySelector("#existing");
+const checkbox = document.querySelector("#checkbox");
+
+const savedUsername = localStorage.getItem("username");
+const savedPassword = localStorage.getItem("password");
+
+if(savedUsername && savedPassword){
+	existingBtn.style.display = "block";
+}
+
+submitBtn.addEventListener("click" ,function(){
+
+	const username = document.querySelector("#username").value;
+
+	alert(`Logged in as ${username}`);
+})
